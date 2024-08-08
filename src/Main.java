7df98c8afbd7d49703890aaf2853e5cb875880c0
erjanisaev42@gmail.com <erjanisaev42@gmail.com>
2024-08-08 10:46:45 +0600

    Конец проекта

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        FileWriter fw = new FileWriter("users.txt");
+        System.out.println("Введите ваше имя");
+        String userName = scanner.next();
+        System.out.println("Введите ваш возраст");
+        int userAge = scanner.nextInt();
+        fw.write(userName);
+        fw.write(": " +userAge);
+        fw.close();
 
+
+        FileReader fr = new FileReader("users.txt");
+        Scanner scanner1 = new Scanner(fr);
+        while (scanner1.hasNextLine()){
+            System.out.println(scanner1.nextLine());
+        }
+        fr.close();
     }
 }
